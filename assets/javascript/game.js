9826478e86339dffec09f15b496239a12c514020
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-06-01 16:31:28 -0700

    basic functionality of hangman now works'

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,13 +24,18 @@ const hangmanWords = [
     'The Day That Never Comes'
 ]
 
+var currentWord = document.getElementById('currentWord');//gets the current word ouputed to screen
+var numGuesses = document.getElementById('numberOfGuesses'); //guess num of gueses output to screen
+var winsOuput = document.getElementById('wins'); //total wins ouput
+var wordString = '';//holds the string that will be ouput to screen
+var randomWord;
+
 //functions makes something happen when a key is pushed
 document.onkeyup = function (e) {
-    var currentWord = document.getElementById('currentWord');
-    var wordString = '';
-    var randomWord = hangmanWords[Math.floor(Math.random() * hangmanWords.length)];
 
+    //initializes game
     if (currentWord.textContent === '') {
+        randomWord = hangmanWords[Math.floor(Math.random() * hangmanWords.length)]; //get random word from array
 
         for (var i = 0; i < randomWord.length; i++) {
             if (randomWord[i] === ' ') {
@@ -40,6 +45,30 @@ document.onkeyup = function (e) {
             }
         }
         alert(randomWord);
-        currentWord.textContent = wordString; 
+        currentWord.textContent = wordString;
+        numGuesses.textContent = numOfGuesses.toString();
+        letterChecker(e.key);
+    } else {
+        letterChecker(e.key);
+        if (!wordString.includes('_')) {
+            winTotal++;
+            winsOuput.textContent = winTotal;
+        }
     }
 };
+
+let letterChecker = function (letter) {
+    if (randomWord.includes(letter.toLowerCase()) ||
+        randomWord.includes(letter.toUpperCase())) {
+        for (var i = 0; i < randomWord.length; i++) {
+            if (randomWord[i] === letter.toLowerCase() ||
+                randomWord[i] === letter.toUpperCase()) {
+                wordString = wordString.substr(0, i) + letter + wordString.substr(i + 1);
+            }
+        }
+        currentWord.textContent = wordString;
+    } else {
+        numOfGuesses--;
+        numGuesses.textContent = numOfGuesses.toString();
+    }
+}
