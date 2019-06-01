08e8c5a822ed0ea3f3e511a90492750c11bf9cb5
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-06-01 15:28:08 -0700

    adds the needed global variables and creates function to do something when button pushed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,30 @@
-var event = document.onkeyup();
+//holds the total number of wins
+var winTotal = 0;
+
+//holds number of guesses
+var numOfGuesses = 15;
+
+//holds letters already guessed
+var lettersGuessed = [];
+
+//Songs that can be guessed
+const hangmanWords = [
+    'Enter Sandman',
+    'Of Wolf and Man',
+    'Moth Into Flame',
+    'Metal Militia',
+    'All Nightmare Long',
+    'Trapped Under Ice',
+    'The Unforgiven',
+    'Bleeding Me',
+    'My Friend of Misery',
+    'Master of Puppets',
+    'Fuel',
+    'Until it Sleeps',
+    'The Day That Never Comes'
+]
+
+//functions makes something happen when a key is pushed
+document.onkeyup = function (e) {
+    alert(e.key);
+};
